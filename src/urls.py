1ca6a8f1b57784dcaf9734334013625507014d86
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-04-29 16:28:35 +0500

    aaaa

--- src/urls.py
+++ src/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("api.v1.urls"))
+    path("api/v1/", include("api.urls"))
 
 ]
